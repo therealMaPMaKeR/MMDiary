e5461839ee0815215f802729e0518edf4469fc34
therealMaPMaKeR <stonedmapster@gmail.com>
2025-08-23 19:36:02 -0400

    deploy 4.2.0 BETA

--- constants.cpp
+++ constants.cpp
@@ -5,7 +5,7 @@
 namespace Constants
 {
 // Global
-const QString AppVer = "4.2.0";
+const QString AppVer = "4.2.0 BETA";
 // Error Messages
 const QString ErrorMessage_Default = "ERROR";
 const QString ErrorMessage_INVUSER = "ERROR - INVALID USER";
