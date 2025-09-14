28e0c41d2decabebb324e8d5151e81ba433af423
therealMaPMaKeR <stonedmapster@gmail.com>
2025-09-14 16:02:27 -0400

    quick

--- constants.cpp
+++ constants.cpp
@@ -5,7 +5,7 @@
 namespace Constants
 {
 // Global
-const QString AppVer = "4.5.3";
+const QString AppVer = "4.5.4";
 // Error Messages
 const QString ErrorMessage_Default = "ERROR";
 const QString ErrorMessage_INVUSER = "ERROR - INVALID USER";
